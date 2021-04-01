8977711fd6d896f6b87182db9d6714cf41a2607c
aydın tarım <cagrihanaydin@hotmail.com>
2021-04-01 10:27:25 +0300

    01.04 Ef refactoring

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
+using Entities.Abstract;
 using Entities.Concrete;
 
 namespace DataAccess.Abstract
-{
-	public interface IEntityRepository<T>
-	{   //default public,filtreleme yapmak için expression geçtik
+{   //generic constraint
+	public interface IEntityRepository<T> where T:class,IEntity,new()
+	{  
 
 		List<T> GetAll(Expression<Func<T,bool>> filter=null);
 
--- DataAccess/Concrete/EntityFramework/EfCarDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -4,6 +4,7 @@ using System.Linq.Expressions;
 using System.Text;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.Concrete.EntityFramework
 {
@@ -11,12 +12,18 @@ namespace DataAccess.Concrete.EntityFramework
 	{
 		public void Add(Car entity)
 		{
-			throw new NotImplementedException();
+			using ReCapContext context = new ReCapContext();
+			var addedEntity = context.Entry(entity);
+			addedEntity.State = EntityState.Added;
+			context.SaveChanges();
 		}
 
 		public void Delete(Car entity)
 		{
-			throw new NotImplementedException();
+			using ReCapContext context = new ReCapContext();
+			var deletedEntity = context.Entry(entity);
+			deletedEntity.State = EntityState.Deleted;
+			context.SaveChanges();
 		}
 
 		public Car Get(Expression<Func<Car, bool>> filter)
@@ -31,7 +38,10 @@ namespace DataAccess.Concrete.EntityFramework
 
 		public void Update(Car entity)
 		{
-			throw new NotImplementedException();
+			using ReCapContext context = new ReCapContext();
+			var updatedEntity = context.Entry(entity);
+			updatedEntity.State = EntityState.Modified;
+			context.SaveChanges();
 		}
 	}
 }
